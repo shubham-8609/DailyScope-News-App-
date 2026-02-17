e5445ecf40f47fdc2c3acfcd02706d987e5aab3e
codeleg <shubhamgupta86090@gmail.com>
2026-02-17 17:56:29 +0530

    Add Navigation component

---
+++ app/src/main/java/com/codeleg/dailyscope/ui/activity/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.codeleg.dailyscope.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import com.codeleg.dailyscope.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.time.delay
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.nav_host_fragment)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        lifecycleScope.launch {
+            delay(1500)
+            findNavController(R.id.nav_host_fragment).navigate(R.id.action_homeFragment_to_articleFragment)
+        }
+    }
+}
