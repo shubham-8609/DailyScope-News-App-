1c1320af49b0b56edb9bff750194300d5196c60c
codeleg <shubhamgupta86090@gmail.com>
2026-03-02 22:47:23 +0530

    Update Home fragment layout and news/article layout

--- app/src/main/java/com/codeleg/dailyscope/database/model/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.codeleg.dailyscope.database.model
-
-data class NewsResponse(
-    val message: String,
-    val news: List<News>,
-    val success: Boolean
-)
--- app/src/main/java/com/codeleg/dailyscope/database/model/Source.kt
+++
@@ -1,8 +0,0 @@
-package com.codeleg.dailyscope.database.model
-
-data class Source(
-    val hostname: String,
-    val id: String,
-    val name: String?,
-    val website: String?
-)
